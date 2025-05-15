e2b8c93e27fe341d04b06c1677d6370edae6ab1a
5000user5000 <ck10600760@gmail.com>
2025-05-15 17:05:08 +0800

    docs: add some comment

--- HDLBits/tb2.sv
+++ HDLBits/tb2.sv
@@ -1,8 +1,12 @@
 module top_module();
+
+    // variables declaration
     reg clk,in;
     reg [2:0] s;
     wire out;
-	
+
+	// Instantiate the DUT
+    // DUT: q7
     q7 uut(
         .clk(clk),
         .in(in),
@@ -10,14 +14,13 @@ module top_module();
         .out(out)
     );
 
-    // clock: 10-unit period
+    // Clock: 10-unit period
     initial clk = 0;
     always #5 clk = ~clk;
     
     // Input stimulus for in
     initial begin
         in = 0;
-
         #20 in = 1;
         #10 in = 0;
         #10 in = 1;
