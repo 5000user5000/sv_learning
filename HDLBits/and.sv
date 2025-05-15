7408faadc68e7eefbc552ebca7621302d452ba2f
5000user5000 <ck10600760@gmail.com>
2025-05-15 16:45:59 +0800

    fix: use reg for input and wire for output

--- HDLBits/and.sv
+++ HDLBits/and.sv
@@ -1,11 +1,19 @@
 module top_module();
-    intput[1:0] in;
-    output out;
+    reg[1:0] in;
+    wire out;
+
+    // Instantiate the AND gate
+    andgate uut (
+        .in(in),
+        .out(out)
+    );
+
+
 	initial begin
         in = 2'b0;
         #10 in[0] = 1;
         #10 in = ~in;
         #10 in[0] = 1;
     end
-    andgate( .in(in) , .out(out) );
+    
 endmodule
